d0e2aa2098efd5d3520d6db08118a357e1ddae52
Damar707 <81214793+Damar707@users.noreply.github.com>
2022-10-04 05:33:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ global.owner = [
   ['6282221792667', 'TioXd', true]
 // [owner bukan bot!]
 ] // paste nomor di sini
-global.mods = ['6282221792667'] // bantuan?
-global.prems = ['6282221792667'] // user premium limit tanpa batas 
+global.mods = ['6283867228380'] // bantuan?
+global.prems = ['6283867228380'] // user premium limit tanpa batas 
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrttm: 'https://nurutomo.herokuapp.com',
@@ -20,9 +20,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Wm untuk stiker dan autobio
-global.tio = 'TioXd'
-global.packname = '© BOTCAHX'
-global.author = 'TioXd'
+global.tio = 'DamarHaxoer'
+global.packname = '© DmrHxr'
+global.author = 'DamarHaxoer'
 
 global.multiplier = 70 // Semakin Tinggi levelmu semakin naik
 
